268a983b7430571452b9f532627ee73e98679a43
hpu-hyh <2787281908@qq.com>
2022-06-05 13:10:56 +0800

    Template

---
+++ js/Template.js
@@ -0,0 +1,33 @@
+let images = document.querySelector('.images')
+let image = document.querySelector('.image')
+for(let i = 1;i<48;i++){
+    var lis = document.createElement('div')
+    lis.innerHTML=`<img class="img" src='./image/img/img (`+i+`).png'>`
+    images.appendChild(lis)
+}
+let img = document.querySelectorAll('.img')
+// for(let i = 0;i<img.length;i++){
+//     img[i].addEventListener('click',()=>{
+//         image.style.display='flex'
+//         let index = i+1
+//         image.innerHTML="<img src='./image/img/img (" + index + ").png'>"
+//         document.body.style.overflow='hidden'//禁止页面滑动
+//     })
+// }
+// image.addEventListener('click',()=>{
+//     image.style.display='none'
+//     document.body.style.overflow='auto'//出现滚动条
+// })
+
+for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener("click", () => {
+        image.style.display = "flex"
+        let index = i + 1
+        image.innerHTML = "<img src='./image/img/img (" + index + ").png'>"
+        document.body.style.overflow = 'hidden'; //禁止页面滑动
+    })
+}
+image.addEventListener('click', () => {
+    image.style.display = "none"
+    document.body.style.overflow = 'auto'; //出现滚动条
+})
